513364bc1c04dce2ac187b175017e18fec4353b1
Chris Midgley <chris.midgley@newredo.com>
2022-05-28 14:34:20 +0100

    closer to Rhino

--- Main.java
+++ Main.java
@@ -1,11 +1,17 @@
 import java.io.IOException;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.nio.file.Path;
+import java.net.URL;
+import java.net.URLConnection;
 
 class Main {
-  public static void main(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException, MalformedURLException {
     Path file = Paths.get("index.js");
     System.out.println(Files.probeContentType(file));
+    URL url = file.toUri().toURL();
+    URLConnection urlConnection = url.openConnection();
+    System.out.println(urlConnection.getContentType());
   }
 }
